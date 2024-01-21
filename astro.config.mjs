ceea2eccdfbfafcd0c0eb4e4734e226e6a0aa0fb
Santiago Betancur <santibeta82@gmail.com>
2024-01-20 19:27:57 -0500

    Add vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,9 @@ import react from "@astrojs/react";
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: "hybrid",
-  adapter: vercel(),
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
